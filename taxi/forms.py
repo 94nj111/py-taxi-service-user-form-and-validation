05a0b1ca272164e7042840851f92354f0fd1cc5a
Dmytro <94nj111@gmail.com>
2024-11-24 22:30:19 +0200

    *

--- taxi/forms.py
+++ taxi/forms.py
@@ -14,7 +14,7 @@ class DriverCreationForm(UserCreationForm):
         validators=[
             RegexValidator(
                 regex=REGEX,
-                message="Enter a valid value: the appropriate format is 'AAA00000'",
+                message="The appropriate format is 'AAA00000'",
             ),
         ],
     )
@@ -36,7 +36,7 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         validators=[
             RegexValidator(
                 regex=REGEX,
-                message="Enter a valid value: the appropriate format is 'AAA00000'",
+                message="The appropriate format is 'AAA00000'",
             ),
         ],
     )
